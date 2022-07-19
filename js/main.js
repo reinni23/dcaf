453c9a41fa9fdb9b17ed0a69abd6d57bc75b118d
reinni23 <reinni@naver.com>
2022-07-19 14:04:44 +0900

    220719 수정

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
     var scroll = $(window).scrollTop();
     if (scroll > 160) {
       $(".h_nav").css("background", "#000");
-      $(".h_nav a").css("color", "#fff");
+      $(".h_nav>ul>li>a").css("color", "#fff");
       $(".h_view>span").css("background", "#fff");
       $(".logo_nav").css("display", "block");
       $("#header").addClass("fixed");
       $(".form").css("display", "none");
     } else {
       $(".h_nav").css("background", "#fff");
-      $(".h_nav a").css("color", "#000");
+      $(".h_nav>ul>li>a").css("color", "#000");
       $(".h_view>span").css("background", "#000");
       $(".logo_nav").css("display", "none");
       $("#header").removeClass("fixed");
@@ -27,8 +27,21 @@ $(document).ready(function () {
   });
 });
 
+//문화에술공간 nav
+$(".art_nav_i").click(function () {
+  if ($(".artpage").css("display") === "none") {
+    $(".artpage").show();
+  } else {
+    $("artpage").hide();
+  }
+});
+
+$(".nav_close").click(function () {
+  $(".artpage").hide();
+});
+
 var swiper = new Swiper(".Scrollbar", {
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
